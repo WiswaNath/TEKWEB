25967a2cbff27d83da941b804f883bcbc7b07c39
WiswaNath <127913181+WiswaNath@users.noreply.github.com>
2023-09-28 20:14:27 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        ngempit:'-0.5em',
+        wiswa: '.5em',
+      }
+    },
+  },
+  plugins: [],
+}
+
